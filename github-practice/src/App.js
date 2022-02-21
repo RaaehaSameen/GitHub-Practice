73e0633a58011e43826a4c447ee1cd7ec99334b8
RaaehaSameen <raaehaquazi93@gmail.com>
2022-02-21 18:09:55 +0530

    RQ-Commit changes

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Raaeha from "./Components/RAAEHA/Raaeha";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Raaeha />
     </div>
   );
 }
---
+++ github-practice/src/Components/RAAEHA/Raaeha.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Raaeha = () => {
+    return (
+        <div>
+            <h1>I am Raaeha Quazi</h1>
+        </div>
+    );
+};
+
+export default Raaeha;
